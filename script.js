60cd2a1840694e6fb8643a6384d7a68bdab0e10e
Andrew Yu <andrewyu209@gmail.com>
2021-10-14 01:18:23 -0400

    Add getUserOptions to it's own function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Assignment code here
+// Array of special characters to be included in password
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 // Function to Enter a valid length or Loop it until it is valid
 function passwordLength() 
 {
@@ -8,38 +16,37 @@ function passwordLength()
     window.alert("You need to provide a valid length. Please try again");
     return passwordLength();
   }
-  while(length < 8 || length > 128)
+  else if(length < 8 || length > 128)
   {
     window.alert("Please provide a length greater than 8 and less than 128");
     return passwordLength();
   }
   return parseInt(length);
 }
-
-// Generate Password Function
-var generatePassword = function() 
+// Get users Criteria (If all are false Loop again)
+function getUsersOption() 
 {
-  // storing all the lower/upper/number/special
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric = "0123456789";
-  var special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  var password = 
-  {
-    length: passwordLength(),
+  var option = {
     hasNumber: window.confirm("Would you like to include Numbers?"),
     hasLowerCase: window.confirm("Would you like to include Lower Case letters?"),
     hasUpperCase: window.confirm("Would you like to include Upper Case letters?"),
-    hasSpecialChar: window.confirm("Would you like to include Special Characters?"),
-    result: ""
+    hasSpecialChar: window.confirm("Would you like to include Special Characters?")
   }
+  if(option.hasLowerCase === false && option.hasUpperCase === false && option.hasNumber === false && option.hasSpecialChar === false)
+  {
+    window.alert("Password needs to include at least one of the following Options.");
+    return getUsersOption();
+  }
+  return option;
+}
+// Generate Password Function
+var generatePassword = function() 
+{
+  var length = passwordLength();
+  var options = getUsersOption();
+  var password = "";
   debugger;
   // If all hasNumber/hasLowerCase/hasUpperCase/hasSpecialChar is false, give an error and start the function again
-  if(password.hasLowerCase == false && password.hasUpperCase == false && password.hasNumber == false && password.hasSpecialChar == false)
-  {
-    console.log("Password needs at least lower case/Upper case/ number/ or special Characters");
-    return generatePassword();
-  }
   return password.result;
 }
 
